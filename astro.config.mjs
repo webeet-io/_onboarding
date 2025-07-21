cce751eab3607e41ad2b01054b008d0864763263
mstarkcode <mstarkcode@gmail.com>
2025-07-21 10:42:42 +0200

    feat(astro.config): implement auto sidebar for multiple domains in sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import starlightUtils from "@lorenzo_lewis/starlight-utils";
+import starlightAutoSidebar from "starlight-auto-sidebar";
 import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
@@ -13,13 +13,7 @@ export default defineConfig({
 
     starlight({
       title: "Webeet Docs",
-      plugins: [
-        starlightUtils({
-          multiSidebar: {
-            switcherStyle: "hidden",
-          },
-        }),
-      ],
+      plugins: [starlightAutoSidebar()],
       customCss: ["./src/styles/app.css"],
       social: [
         {
@@ -35,14 +29,17 @@ export default defineConfig({
       sidebar: [
         {
           label: "SWE",
+          collapsed: true,
           autogenerate: { directory: "onboarding/swe" },
         },
         {
           label: "Data",
+          collapsed: true,
           autogenerate: { directory: "onboarding/data" },
         },
         {
           label: "DevSecOps",
+          collapsed: true,
           autogenerate: { directory: "onboarding/devsecops" },
         },
       ],
